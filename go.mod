b709dd5fe1f4270c5c07996953f5c85126f8992c
Andrew Wansley <andrew.wansley@gmail.com>
2020-03-27 13:05:33 -0700

    Experiment with using protobufs for data types

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module awans.org/aft
 go 1.14
 
 require (
+	github.com/golang/protobuf v1.4.0-rc.4
 	github.com/gorilla/mux v1.7.4
 	github.com/lithammer/shortuuid/v3 v3.0.4
+	google.golang.org/protobuf v1.20.1
 )
--- internal/server/main.go
+++ internal/server/main.go
@@ -14,20 +14,20 @@ var objects = []data.Object{
 	data.Object{
 		Id:   "Cekw67uyMpBGZLRP2HFVbe",
 		Name: "Test",
-		Fields: []data.Field{
-			data.Field{
+		Fields: []*data.Field{
+			&data.Field{
 				Name: "f1",
-				Type: data.Text,
+				Type: data.FieldType_TEXT,
 			},
 		},
 	},
 	data.Object{
 		Id:   "6R7VqaQHbzC1xwA5UueGe6",
 		Name: "Cool",
-		Fields: []data.Field{
-			data.Field{
+		Fields: []*data.Field{
+			&data.Field{
 				Name: "f5",
-				Type: data.Int,
+				Type: data.FieldType_INT,
 			},
 		},
 	},
