aec10b6afd5f0fee9f1ddbf8580bbf15670aec42
Andrew Wansley <andrew.wansley@gmail.com>
2020-05-27 16:07:19 -0700

    Use a pointer reciever for router so we can modify endpoint

--- internal/server/router.go
+++ internal/server/router.go
@@ -11,25 +11,25 @@ type router struct {
 	entrypoint http.Handler
 }
 
-func NewRouter() router {
+func NewRouter() *router {
 	r := router{}
 	r.r = mux.NewRouter().Methods("POST").Subrouter()
 	r.entrypoint = r.r
-	return r
+	return &r
 }
 
-func (r router) AddMiddleware(middleware []lib.Middleware) {
+func (r *router) AddMiddleware(middleware []lib.Middleware) {
 	for _, m := range middleware {
 		r.entrypoint = m(r.entrypoint)
 	}
 }
 
-func (r router) AddRoutes(routes []lib.Route) {
+func (r *router) AddRoutes(routes []lib.Route) {
 	for _, route := range routes {
 		r.r.Path(route.Pattern).Name(route.Name).Handler(route.Handler)
 	}
 }
 
-func (r router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.entrypoint.ServeHTTP(w, req)
 }
