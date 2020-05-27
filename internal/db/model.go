f387c0424a1f944a62f42b96fc6bfdb6655bdc12
Chase <chase.hensel@gmail.com>
2020-05-26 22:53:15 -0700

    support blank email addresses

--- internal/db/model.go
+++ internal/db/model.go
@@ -74,7 +74,7 @@ func (a Attribute) ParseFromJson(value interface{}) (interface{}, error) {
 	    if ok {
 	    	//https://www.alexedwards.net/blog/validation-snippets-for-go#email-validation
 			var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-			if len(emailAddressString) > 254 || !rxEmail.MatchString(emailAddressString) {
+			if (len(emailAddressString) > 254 || !rxEmail.MatchString(emailAddressString)) && len(emailAddressString) != 0 {
 				return nil, fmt.Errorf("expected email address got %v", emailAddressString)
 			}
 		} else {
