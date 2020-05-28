a196b0263cdf9258f9285a10ff59b203855c202d
Chase <chase.hensel@gmail.com>
2020-05-27 17:57:44 -0700

    Add datatype package

---
+++ internal/datatypes/email.go
@@ -0,0 +1,13 @@
+package datatypes
+
+import (
+	"regexp"
+)
+
+//https://www.alexedwards.net/blog/validation-snippets-for-go#email-validation
+var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+
+func MatchEmail(s string) bool {
+	return rxEmail.MatchString(s);
+}
