315a9abbfc6ad11ad3fd16a46ce5f005b08aba19
Chase <chase.hensel@gmail.com>
2020-06-03 15:27:28 -0700

    Fix broken tests

--- internal/db/datatype_test.go
+++ internal/db/datatype_test.go
@@ -9,74 +9,74 @@ import (
 func TestBool(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = boolFromJSONFunc(false)
+	r, _ = boolValidatorFunc(false)
 	assert.Equal(t, r, false)
-	r, err = boolFromJSONFunc("bad data")
+	r, err = boolValidatorFunc("bad data")
 	assert.Error(t, err)
-	r, err = boolFromJSONFunc(0)
+	r, err = boolValidatorFunc(0)
 	assert.Error(t, err)
 }
 
 func TestInt(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = intFromJSONFunc(1)
+	r, _ = intValidatorFunc(1)
 	assert.Equal(t, r, int64(1))
-	r, err = intFromJSONFunc("bad data")
+	r, err = intValidatorFunc("bad data")
 	assert.Error(t, err)
-	r, err = intFromJSONFunc(0.6)
+	r, err = intValidatorFunc(0.6)
 	assert.Equal(t, r, int64(0))
-	r, err = intFromJSONFunc(int(6))
+	r, err = intValidatorFunc(int(6))
 	assert.Equal(t, r, int64(6))
 }
 
 func TestEnum(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = enumFromJSONFunc(1)
+	r, _ = enumValidatorFunc(1)
 	assert.Equal(t, r, int64(1))
-	r, err = enumFromJSONFunc("bad data")
+	r, err = enumValidatorFunc("bad data")
 	assert.Error(t, err)
-	r, err = enumFromJSONFunc(0.6)
+	r, err = enumValidatorFunc(0.6)
 	assert.Equal(t, r, int64(0))
-	r, err = enumFromJSONFunc(int(6))
+	r, err = enumValidatorFunc(int(6))
 	assert.Equal(t, r, int64(6))
 }
 
 func TestString(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = stringFromJSONFunc("this is a test")
+	r, _ = stringValidatorFunc("this is a test")
 	assert.Equal(t, r, "this is a test")
-	r, err = stringFromJSONFunc(5)
+	r, err = stringValidatorFunc(5)
 	assert.Error(t, err)
-	r, _ = stringFromJSONFunc("5")
+	r, _ = stringValidatorFunc("5")
 	assert.Equal(t, r, "5")
 }
 
 func TestText(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = textFromJSONFunc("this is a test")
+	r, _ = textValidatorFunc("this is a test")
 	assert.Equal(t, r, "this is a test")
-	r, err = textFromJSONFunc(5)
+	r, err = textValidatorFunc(5)
 	assert.Error(t, err)
-	r, _ = textFromJSONFunc("5")
+	r, _ = textValidatorFunc("5")
 	assert.Equal(t, r, "5")
 }
 
 func TestEmailAddress(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = emailAddressFromJSONFunc("andrew.wansley@gmail.com")
+	r, _ = emailAddressValidatorFunc("andrew.wansley@gmail.com")
 	assert.Equal(t, r, "andrew.wansley@gmail.com")
-	r, err = emailAddressFromJSONFunc("wansley")
+	r, err = emailAddressValidatorFunc("wansley")
 	assert.Error(t, err)
-	r, err = emailAddressFromJSONFunc("wansley@")
+	r, err = emailAddressValidatorFunc("wansley@")
 	assert.Error(t, err)
-	r, err = emailAddressFromJSONFunc("wansley@.c")
+	r, err = emailAddressValidatorFunc("wansley@.c")
 	assert.Error(t, err)
-	r, err = emailAddressFromJSONFunc("chase@a.co")
+	r, err = emailAddressValidatorFunc("chase@a.co")
 	assert.NoError(t, err)
 }
 
@@ -84,42 +84,42 @@ func TestUUID(t *testing.T) {
 	var r interface{}
 	var err error
 
-	r, _ = uuidFromJSONFunc("d9c59e23-e050-4fc7-949d-8535ae8e3a49")
+	r, _ = uuidValidatorFunc("d9c59e23-e050-4fc7-949d-8535ae8e3a49")
 	u, _ := uuid.Parse("d9c59e23-e050-4fc7-949d-8535ae8e3a49")
 	assert.Equal(t, r, u)
-	r, err = uuidFromJSONFunc("d9c59e23-e050-4fc7-949d-8535ae8e3a4")
+	r, err = uuidValidatorFunc("d9c59e23-e050-4fc7-949d-8535ae8e3a4")
 	assert.Error(t, err)
-	r, err = uuidFromJSONFunc(123123)
+	r, err = uuidValidatorFunc(123123)
 	assert.Error(t, err)
 	u, _ = uuid.Parse("39804353-56ee-463e-9b05-5e916ea293bd")
-	r, _ = uuidFromJSONFunc(u)
+	r, _ = uuidValidatorFunc(u)
 	assert.Equal(t, r, u)
 }
 
 func TestFloat(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = floatFromJSONFunc(1)
+	r, _ = floatValidatorFunc(1)
 	assert.Equal(t, r, float64(1))
-	r, err = floatFromJSONFunc("bad data")
+	r, err = floatValidatorFunc("bad data")
 	assert.Error(t, err)
-	r, err = floatFromJSONFunc(0.6)
+	r, err = floatValidatorFunc(0.6)
 	assert.Equal(t, r, 0.6)
-	r, err = floatFromJSONFunc(int(6))
+	r, err = floatValidatorFunc(int(6))
 	assert.Equal(t, r, float64(6))
 }
 
 func TestURL(t *testing.T) {
 	var r interface{}
 	var err error
-	r, _ = URLFromJSONFunc("https://www.google.com")
+	r, _ = URLValidatorFunc("https://www.google.com")
 	assert.Equal(t, r, "https://www.google.com")
-	r, _ = URLFromJSONFunc("https://www.google.comdaddy")
+	r, _ = URLValidatorFunc("https://www.google.comdaddy")
 	assert.Equal(t, r, "https://www.google.comdaddy")
-	r, err = URLFromJSONFunc("www.google.com")
+	r, err = URLValidatorFunc("www.google.com")
 	assert.Error(t, err)
-	r, err = URLFromJSONFunc("localhost:8080")
+	r, err = URLValidatorFunc("localhost:8080")
 	assert.Error(t, err)
-	r, err = URLFromJSONFunc(5)
+	r, err = URLValidatorFunc(5)
 	assert.Error(t, err)
 }
