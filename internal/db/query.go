150abaa33db5aaf8c002c003b89b3b32810e86b7
Andrew Wansley <andrew.wansley@gmail.com>
2020-06-16 16:15:04 -0700

    Implement None/Every aggregations

--- internal/db/query.go
+++ internal/db/query.go
@@ -221,7 +221,7 @@ func getRelatedOne(tx Tx, rec Record, j join, matcher Matcher) *QueryResult {
 	panic("invalid join")
 }
 
-func (q Q) performJoinManySome(outer []*QueryResult, j join) []*QueryResult {
+func (q Q) performJoinManySomeOrInclude(outer []*QueryResult, j join, a Aggregation) []*QueryResult {
 	key := j.Key()
 	matchers := q.sargs[j.to.aliasID]
 	var inner [][]*QueryResult
@@ -271,7 +271,177 @@ func (q Q) performJoinManySome(outer []*QueryResult, j join) []*QueryResult {
 			}
 		}
 		if isEmpty {
-			outer[i].Empty()
+			// if this is a Some aggregation
+			// blank out the parent record
+			if a == Some {
+				outer[i].Empty()
+			}
+		} else {
+			dict := outer[i].ToMany
+			if dict != nil {
+				dict[key] = populatedJoinedSet
+			} else {
+				outer[i].ToMany = map[string][]*QueryResult{key: populatedJoinedSet}
+			}
+		}
+
+	}
+	return outer
+}
+
+func (q Q) performJoinManyNone(outer []*QueryResult, j join, a Aggregation) []*QueryResult {
+	var inner [][]*QueryResult
+	for _, r := range outer {
+		qr := getRelatedMany(q.tx, r.Record, j, nil)
+		inner = append(inner, qr)
+	}
+
+	matchers := q.sargs[j.to.aliasID]
+	matcher := And(matchers...)
+
+	// apply the local filtering criteria
+	// eagerly so we can maybe avoid doing some extra joins
+	var filtered [][]*QueryResult
+	for _, group := range inner {
+		none := true
+		for _, result := range group {
+			match, _ := matcher.Match(result.Record)
+			if match {
+				none = false
+				break
+			}
+		}
+		if none {
+			filtered = append(filtered, group)
+		} else {
+			filtered = append(filtered, []*QueryResult{})
+		}
+	}
+
+	// to prevent explosion, we first merge by unique records
+	// and then expand out
+	uniq := map[uuid.UUID]*QueryResult{}
+	for _, group := range inner {
+		for _, result := range group {
+			uniq[result.Record.ID()] = result
+		}
+	}
+
+	// just copy out the unique values
+	var uniqValues []*QueryResult
+	for _, uniqVal := range uniq {
+		uniqValues = append(uniqValues, uniqVal)
+	}
+
+	// do all of the child joins
+	// we're passing pointers so stuff'll get modified in-place in the dict
+	q.performJoins(uniqValues, j.to.aliasID)
+
+	// merge 'em back, filtering if any make it back
+	for i := range outer {
+		joinedSet := inner[i]
+		none := true
+		for _, joined := range joinedSet {
+			if !joined.isEmpty() {
+				populated := uniq[joined.Record.ID()]
+				if !populated.isEmpty() {
+					none = false
+					break
+				}
+			}
+		}
+
+		// okay cool, now populated joined set contains fully joined values
+		// for this input record
+		// apply the aggregation!
+		if !none {
+			// if this is an None aggregation
+			// blank out the parent record
+			if a == None {
+				outer[i].Empty()
+			}
+		}
+	}
+	return outer
+}
+
+func (q Q) performJoinManyEvery(outer []*QueryResult, j join, a Aggregation) []*QueryResult {
+	key := j.Key()
+	var inner [][]*QueryResult
+	for _, r := range outer {
+		qr := getRelatedMany(q.tx, r.Record, j, nil)
+		inner = append(inner, qr)
+	}
+
+	matchers := q.sargs[j.to.aliasID]
+	matcher := And(matchers...)
+
+	// apply the local filtering criteria
+	var filtered [][]*QueryResult
+	// eagerly so we can maybe avoid doing some extra joins
+	for _, group := range inner {
+		every := true
+		for _, result := range group {
+			match, _ := matcher.Match(result.Record)
+			if !match {
+				every = false
+				break
+			}
+		}
+		if every {
+			filtered = append(filtered, group)
+		} else {
+			filtered = append(filtered, []*QueryResult{})
+		}
+	}
+
+	// to prevent explosion, we first merge by unique records
+	// and then expand out
+	uniq := map[uuid.UUID]*QueryResult{}
+	for _, group := range inner {
+		for _, result := range group {
+			uniq[result.Record.ID()] = result
+		}
+	}
+
+	// just copy out the unique values
+	var uniqValues []*QueryResult
+	for _, uniqVal := range uniq {
+		uniqValues = append(uniqValues, uniqVal)
+	}
+
+	// do all of the child joins
+	// we're passing pointers so stuf'll get modified in-place in the dict
+	q.performJoins(uniqValues, j.to.aliasID)
+
+	// merge 'em back, filtering if any didn't make it back
+	for i := range outer {
+		joinedSet := inner[i]
+		every := true
+		var populatedJoinedSet []*QueryResult
+		for _, joined := range joinedSet {
+			if !joined.isEmpty() {
+				populated := uniq[joined.Record.ID()]
+				if populated.isEmpty() {
+					every = false
+					break
+				}
+				populatedJoinedSet = append(populatedJoinedSet, populated)
+			} else {
+				every = false
+				break
+			}
+		}
+
+		// okay cool, now populated joined set contains fully joined values
+		// for this input record
+		// apply the aggregation!
+		if !every {
+			// if this is an Every aggregation
+			// blank out the parent record
+			if a == Every {
+				outer[i].Empty()
+			}
 		} else {
 			dict := outer[i].ToMany
 			if dict != nil {
@@ -292,8 +462,12 @@ func (q Q) performJoinMany(outer []*QueryResult, j join) []*QueryResult {
 		agg = Include
 	}
 	switch agg {
-	case Some:
-		return q.performJoinManySome(outer, j)
+	case Some, Include:
+		return q.performJoinManySomeOrInclude(outer, j, agg)
+	case Every:
+		return q.performJoinManyEvery(outer, j, agg)
+	case None:
+		return q.performJoinManyNone(outer, j, agg)
 	}
 	panic("not implemented")
 }
@@ -302,10 +476,15 @@ func getRelatedMany(tx Tx, rec Record, j join, matcher Matcher) []*QueryResult {
 	b := j.on.b
 	d := b.Dual()
 	id := rec.ID()
+	if matcher != nil {
+		matcher = And(EqFK(d.Name(), id), matcher)
+	} else {
+		matcher = EqFK(d.Name(), id)
+	}
 	switch b.RelType() {
 	case HasMany:
 		// FK on the other side
-		hits := tx.FindMany(d.ModelID(), And(EqFK(d.Name(), id), matcher))
+		hits := tx.FindMany(d.ModelID(), matcher)
 		var results []*QueryResult
 		for _, h := range hits {
 			results = append(results, &QueryResult{Record: h})
