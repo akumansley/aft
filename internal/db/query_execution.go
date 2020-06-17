f83b7122cccaa8e4bd9165ba42aad327edb79f6e
Andrew Wansley <andrew.wansley@gmail.com>
2020-06-17 15:19:22 -0700

    Split query into two files

--- internal/db/query_execution.go
+++ internal/db/query_execution.go
@@ -1,133 +1,9 @@
-// user = db.Ref(modelID)
-// db.Query(user).Join(post, user.Rel["posts"]).Filter(post, db.Eq("foo", "bar")).All()
-
 package db
 
 import (
 	"encoding/json"
 	"github.com/google/uuid"
 )
-
-type Aggregation int
-
-const (
-	Every Aggregation = iota
-	Some
-	None
-	Include
-)
-
-type QueryResult struct {
-	Record Record
-	ToOne  map[string]*QueryResult
-	ToMany map[string][]*QueryResult
-}
-
-func (qr *QueryResult) isEmpty() bool {
-	return qr.Record == nil
-}
-
-func (qr *QueryResult) Empty() {
-	qr.Record = nil
-}
-
-func (qr *QueryResult) MarshalJSON() ([]byte, error) {
-	if qr.Record == nil {
-		return json.Marshal(nil)
-	}
-	data := qr.Record.Map()
-	for k, v := range qr.ToOne {
-		data[k] = v
-	}
-	for k, v := range qr.ToMany {
-		data[k] = v
-	}
-	return json.Marshal(data)
-}
-
-type ModelRef struct {
-	modelID uuid.UUID
-	aliasID uuid.UUID
-	model   Model
-}
-
-type RefBinding struct {
-	from ModelRef
-	b    Binding
-}
-
-func (ref ModelRef) Rel(name string) RefBinding {
-	b, _ := ref.model.GetBinding(name)
-	return RefBinding{ref, b}
-}
-
-type joinType int
-
-const (
-	leftJoin joinType = iota
-	innerJoin
-)
-
-type join struct {
-	to ModelRef
-	on RefBinding
-	jt joinType
-}
-
-func (j join) Key() string {
-	return j.on.b.Name()
-}
-
-func (j join) IsToOne() bool {
-	return j.on.b.RelType() == HasOne || j.on.b.RelType() == BelongsTo
-}
-
-type Q struct {
-	root         ModelRef
-	aggregations map[uuid.UUID]Aggregation
-	joins        map[uuid.UUID][]join
-	tx           *holdTx
-	sargs        map[uuid.UUID][]Matcher
-}
-
-func (tx *holdTx) Ref(modelID uuid.UUID) ModelRef {
-	model, _ := tx.GetModelByID(modelID)
-	return ModelRef{modelID, uuid.New(), model}
-}
-
-func (tx *holdTx) Query(model ModelRef) Q {
-	return Q{root: model, tx: tx,
-		sargs:        map[uuid.UUID][]Matcher{},
-		aggregations: map[uuid.UUID]Aggregation{},
-		joins:        map[uuid.UUID][]join{}}
-}
-
-func (q Q) Join(to ModelRef, on RefBinding) Q {
-	outer := on.from
-	j := join{to, on, innerJoin}
-	joinList, ok := q.joins[outer.aliasID]
-	if ok {
-		q.joins[outer.aliasID] = append(joinList, j)
-	} else {
-		q.joins[outer.aliasID] = []join{j}
-	}
-	return q
-}
-
-func (q Q) Filter(ref ModelRef, m Matcher) Q {
-	matcherList, ok := q.sargs[ref.aliasID]
-	if ok {
-		q.sargs[ref.aliasID] = append(matcherList, m)
-	} else {
-		q.sargs[ref.aliasID] = []Matcher{m}
-	}
-	return q
-}
-
-func (q Q) Aggregate(ref ModelRef, a Aggregation) Q {
-	q.aggregations[ref.aliasID] = a
-	return q
-}
 
 func filterEmpty(results []*QueryResult) []*QueryResult {
 	filtered := []*QueryResult{}
@@ -138,16 +14,6 @@ func filterEmpty(results []*QueryResult) []*QueryResult {
 	}
 	return filtered
 }
-
-func (q Q) All() []*QueryResult {
-	var results []*QueryResult
-	matchers := q.sargs[q.root.aliasID]
-	results = q.performScan(q.root.modelID, And(matchers...))
-	results = q.performJoins(results, q.root.aliasID)
-	results = filterEmpty(results)
-	return results
-}
-
 func (q Q) performScan(modeID uuid.UUID, matcher Matcher) []*QueryResult {
 	recs := q.tx.FindMany(q.root.modelID, matcher)
 	var results []*QueryResult
--- internal/db/query_test.go
+++ internal/db/query_test.go
@@ -1,12 +1,15 @@
 package db
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 )
 
 const (
 	userId1 = "e6053eb9-3c28-4152-a4ca-c582f20fc8f0"
 	userId2 = "f4cc9efe-55cf-4f05-8061-4b3b4dbc8295"
+	userId3 = "d40fab5d-883b-4568-b568-b68e1cbc8292"
 	postId1 = "64e3c9e2-4b4d-4009-8cb9-f8938e135926"
 	postId2 = "7e374648-8a0a-4317-8768-be10f10ab743"
 )
@@ -15,9 +18,18 @@ func addTestData(db DB) {
 	tx := db.NewRWTx()
 	u1 := tx.MakeRecord(User.ID)
 	u1.Set("id", userId1)
+	u1.Set("firstName", "Andrew")
+	u1.Set("age", 32)
 
 	u2 := tx.MakeRecord(User.ID)
 	u2.Set("id", userId2)
+	u2.Set("firstName", "Chase")
+	u2.Set("age", 32)
+
+	u3 := tx.MakeRecord(User.ID)
+	u3.Set("id", userId3)
+	u3.Set("firstName", "Tom")
+	u3.Set("age", 32)
 
 	p1 := tx.MakeRecord(Post.ID)
 	p1.Set("id", postId1)
@@ -29,6 +41,7 @@ func addTestData(db DB) {
 
 	tx.Insert(u1)
 	tx.Insert(u2)
+	tx.Insert(u3)
 	tx.Insert(p1)
 	tx.Insert(p2)
 	tx.Connect(u1, p1, UserPosts)
@@ -37,7 +50,7 @@ func addTestData(db DB) {
 	tx.Commit()
 }
 
-func TestQuery(t *testing.T) {
+func TestQueryJoinMany(t *testing.T) {
 	appDB := New()
 	AddSampleModels(appDB)
 	addTestData(appDB)
@@ -46,6 +59,27 @@ func TestQuery(t *testing.T) {
 	post := tx.Ref(Post.ID)
 	results := tx.Query(user).Join(post, user.Rel("posts")).Filter(post, Eq("text", "hello")).Aggregate(post, Some).All()
 	if len(results) != 1 {
+		t.Error("wrong number of results")
+	}
+}
+
+func TestQueryOr(t *testing.T) {
+	appDB := New()
+	AddSampleModels(appDB)
+	addTestData(appDB)
+	tx := appDB.NewTx()
+	user := tx.Ref(User.ID)
+	post1 := tx.Ref(Post.ID)
+	post2 := tx.Ref(Post.ID)
+
+	results := tx.Query(user).Filter(user, Eq("age", 32)).Or(
+		db.Filter(user, Eq("name", "Andrew")).Join(post1, user.Rel("posts")).Filter(post1, Eq("text", "hello")).Aggregate(post1, Some),
+		db.Filter(user, Eq("name", "Chase")).Join(post2, user.Rel("posts")).Filter(post2, Eq("text", "hello")).Aggregate(post2, None),
+	).All()
+
+	bytes, _ := json.Marshal(results)
+	fmt.Printf("results: %v\n", string(bytes))
+	if len(results) != 2 {
 		t.Error("wrong number of results")
 	}
 }
