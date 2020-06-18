84c85ac88467514554744806a1733171594df507
Andrew Wansley <andrew.wansley@gmail.com>
2020-06-18 09:51:34 -0700

    Plug Or/And/Not into FindMany

--- internal/api/find_many_op.go
+++ internal/api/find_many_op.go
@@ -51,11 +51,13 @@ func (op FindManyOperation) Apply(tx db.Tx) []db.Record {
 func buildQuery(tx db.Tx, op FindManyOperation) db.Q {
 	root := tx.Ref(op.ModelID)
 	q := tx.Query(root)
-	q = handleWhere(tx, q, root, op.Where)
+	qb := q.AsBlock()
+	qb = handleWhere(tx, qb, root, op.Where)
+	q.SetMainBlock(qb)
 	return q
 }
 
-func handleWhere(tx db.Tx, q db.Q, parent db.ModelRef, w Where) db.Q {
+func handleWhere(tx db.Tx, q db.QBlock, parent db.ModelRef, w Where) db.QBlock {
 	for _, fc := range w.FieldCriteria {
 		q = q.Filter(parent, fc.Matcher())
 	}
@@ -65,10 +67,42 @@ func handleWhere(tx db.Tx, q db.Q, parent db.ModelRef, w Where) db.Q {
 	for _, arc := range w.AggregateRelationshipCriteria {
 		q = handleARC(tx, q, parent, arc)
 	}
+
+	var orBlocks []db.QBlock
+	for _, or := range w.Or {
+		orBlock := handleSetOpBranch(tx, parent, or)
+		orBlocks = append(orBlocks, orBlock)
+	}
+	if len(orBlocks) > 0 {
+		q = q.Or(parent, orBlocks...)
+	}
+
+	var andBlocks []db.QBlock
+	for _, and := range w.And {
+		andBlock := handleSetOpBranch(tx, parent, and)
+		andBlocks = append(andBlocks, andBlock)
+	}
+	if len(andBlocks) > 0 {
+		q = q.And(parent, andBlocks...)
+	}
+
+	var notBlocks []db.QBlock
+	for _, not := range w.Not {
+		notBlock := handleSetOpBranch(tx, parent, not)
+		notBlocks = append(notBlocks, notBlock)
+	}
+	if len(notBlocks) > 0 {
+		q = q.Not(parent, notBlocks...)
+	}
 	return q
 }
 
-func handleRC(tx db.Tx, q db.Q, parent db.ModelRef, rc RelationshipCriterion) db.Q {
+func handleSetOpBranch(tx db.Tx, parent db.ModelRef, w Where) db.QBlock {
+	qb := db.NewBlock()
+	return handleWhere(tx, qb, parent, w)
+}
+
+func handleRC(tx db.Tx, q db.QBlock, parent db.ModelRef, rc RelationshipCriterion) db.QBlock {
 	child := tx.Ref(rc.Binding.Dual().ModelID())
 	on := parent.Rel(rc.Binding.Name())
 	q = q.Join(child, on)
@@ -77,7 +111,7 @@ func handleRC(tx db.Tx, q db.Q, parent db.ModelRef, rc RelationshipCriterion) db
 	return q
 }
 
-func handleARC(tx db.Tx, q db.Q, parent db.ModelRef, arc AggregateRelationshipCriterion) db.Q {
+func handleARC(tx db.Tx, q db.QBlock, parent db.ModelRef, arc AggregateRelationshipCriterion) db.QBlock {
 	child := tx.Ref(arc.RelationshipCriterion.Binding.Dual().ModelID())
 	on := parent.Rel(arc.RelationshipCriterion.Binding.Name())
 	q = q.Join(child, on)
