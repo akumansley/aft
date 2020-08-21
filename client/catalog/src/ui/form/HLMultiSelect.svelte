bdf607b02ab0e605e17569b038a371804e3e3797
Andrew Wansley <andrew.wansley@gmail.com>
2020-08-20 19:21:04 -0700

    Add draft UI for roles

---
+++ client/catalog/src/ui/form/HLMultiSelect.svelte
@@ -0,0 +1,27 @@
+<script>
+export let options = [];
+export let selected = [];
+
+const state = {};
+
+function handleChange() {
+	let newVal = [];
+	for (let v of options) {
+		if (state[v.id]) {
+			newVal.push(v)
+		}
+	}
+	selected = newVal;
+}
+
+</script>
+
+<style>
+
+</style>
+{#each options as option}
+	<label>
+	<input type=checkbox bind:checked={state[option.id]} on:change={handleChange}>
+{option.name}
+	</label>
+{/each}
