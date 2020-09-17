490d0e0a698fb13b33c49b453bafcc3ae968dac1
Andrew Wansley <andrew.wansley@gmail.com>
2020-09-16 20:02:05 -0700

    Cleanup unused client model attempts

--- client/catalog/src/api/api.js
+++ client/catalog/src/api/api.js
@@ -1,7 +1,3 @@
-import Many from './Many.svelte';
-import Object from './Object.svelte';
 import ConnectSelect from './ConnectSelect.svelte';
-import { ctx } from './context.js';
-import { mergeOps } from './merge.js';
 
-export {ctx, mergeOps, Many, Object, ConnectSelect}
+export {ConnectSelect}
