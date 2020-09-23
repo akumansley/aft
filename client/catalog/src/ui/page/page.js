f2f5da81be4477ed83b2ad30ad699dd802d323e3
Andrew Wansley <andrew.wansley@gmail.com>
2020-09-22 20:41:18 -0700

    Create module export convenience files for page and spacing

---
+++ client/catalog/src/ui/page/page.js
@@ -0,0 +1,11 @@
+import HLBorder from './HLBorder.svelte';
+import HLContent from './HLContent.svelte';
+import HLHeader from './HLHeader.svelte';
+import HLHeaderItem from './HLHeaderItem.svelte';
+
+export {
+	HLBorder,
+	HLContent,
+	HLHeader,
+	HLHeaderItem,
+}
---
+++ client/catalog/src/ui/spacing/spacing.js
@@ -0,0 +1,4 @@
+import Box from './Box.svelte';
+import HSpace from './HSpace.svelte';
+
+export {Box, HSpace}
