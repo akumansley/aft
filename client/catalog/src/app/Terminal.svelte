7fd1352f6513fc6ccea72f0723356c3e19a4a8f6
Andrew Wansley <andrew.wansley@gmail.com>
2020-09-25 14:54:20 -0700

    Cleanup Terminal

--- client/catalog/src/app/Terminal.svelte
+++ client/catalog/src/app/Terminal.svelte
@@ -1,84 +1,83 @@
 <script>
-import client from '../data/client.js';
-import { terminalStore, navStore } from './stores.js';
-import { getContext } from 'svelte';
+	import client from '../data/client.js';
+	import { terminalStore, navStore } from './stores.js';
+	import { getContext } from 'svelte';
 
-import HLContent from '../ui/page/HLContent.svelte';
-import HLHeader from '../ui/page/HLHeader.svelte';
-import HLRow from '../ui/list/HLRow.svelte';
-import HLBorder from '../ui/page/HLBorder.svelte';
-import HLButton from '../ui/form/HLButton.svelte';
-import CodeMirror from './codemirror/CodeMirror.svelte';
+	import HLContent from '../ui/page/HLContent.svelte';
+	import HLHeader from '../ui/page/HLHeader.svelte';
+	import HLRow from '../ui/list/HLRow.svelte';
+	import HLBorder from '../ui/page/HLBorder.svelte';
+	import HLButton from '../ui/form/HLButton.svelte';
+	import CodeMirror from './codemirror/CodeMirror.svelte';
 
-var terminal;
-var cm;
+	var terminal;
+	var cm;
+	const defaultText = `def main():
+	# Put code to run in here.
+	return "Return what you want to see on the screen."`;
 
-navStore.set("terminal");
+	navStore.set("terminal");
 
-function setUpTerminal() {
-	terminal = getContext("terminal");
-	terminal.setOption("lineNumbers", false);
-	terminal.setOption("readOnly", "nocursor");
-	terminal.setSize(null, 250);
-	terminal.setOption("lint", false);
-}
+	function setUpTerminal(initialized) {
+		terminal = initialized.detail;
+		terminal.setOption("lineNumbers", false);
+		terminal.setOption("readOnly", "nocursor");
+		terminal.setOption("lint", false);
+	}
 
-function setUpCM() {
-	cm = getContext("code");
-	cm.setValue(
-`def main():
-    #Put code to run in here.
-    return "Return what you want to see on the screen."`);
-	cm.setCursor(cm.lastLine(), 1000);
-	terminalStore.subscribe(value => {
-		if ('code' in value) {
-			cm.setValue(value["code"]);
-		}
-		if ('history' in value) {
-			cm.setHistory(value["history"]);
-		}
-		if ('cursor' in value) {
-			cm.setCursor(value["cursor"]);
-		}
-		if ('terminal' in value) {
-			terminal.setValue(value["terminal"]);
-		}
-	});
-	cm.focus();
-}
+	function setUpCM(initialized) {
+		cm = initialized.detail;
+		cm.setValue(defaultText);
+		cm.setCursor(cm.lastLine(), 1000);
+		terminalStore.subscribe(value => {
+			if ('code' in value) {
+				cm.setValue(value["code"]);
+			}
+			if ('history' in value) {
+				cm.setHistory(value["history"]);
+			}
+			if ('cursor' in value) {
+				cm.setCursor(value["cursor"]);
+			}
+			if ('terminal' in value) {
+				terminal.setValue(value["terminal"]);
+			}
+		});
+		cm.focus();
+	}
 
-async function runRepl() {
-	const result = await client.rpc.terminal({args: {data : cm.getValue().trim()}});
-	if(terminal.getValue() == "") {
-		if (result == "") {
-			terminal.setValue(">>> " + cm.getValue().trim());
+	async function runRepl() {
+		const result = await client.rpc.terminal({args: {data : cm.getValue().trim()}});
+		if(terminal.getValue() == "") {
+			if (result == "") {
+				terminal.setValue(">>> " + cm.getValue().trim());
+			} else {
+				terminal.setValue(">>> " + cm.getValue().trim() + "\n" + result);
+			}
+			terminal.setOption("styleActiveLine", true);
 		} else {
-			terminal.setValue(">>> " + cm.getValue().trim() + "\n" + result);
-		}
-		terminal.setOption("styleActiveLine", true);
-	} else {
-		if (result == "") {
-			terminal.setValue(terminal.getValue() + "\n>>> " + cm.getValue().trim());
-		} else {
-			terminal.setValue(terminal.getValue() + "\n>>> " + cm.getValue().trim() + "\n" + result);
-		}
-	} 
-	terminal.setCursor(terminal.lastLine(), 0);
-}
+			if (result == "") {
+				terminal.setValue(terminal.getValue() + "\n>>> " + cm.getValue().trim());
+			} else {
+				terminal.setValue(terminal.getValue() + "\n>>> " + cm.getValue().trim() + "\n" + result);
+			}
+		} 
+		terminal.setCursor(terminal.lastLine(), 0);
+	}
 
-function clearRepl() {
-	terminal.setValue("");
-	terminal.setOption("styleActiveLine", false);
-}
+	function clearRepl() {
+		terminal.setValue("");
+		terminal.setOption("styleActiveLine", false);
+	}
 
-function saveCode(){
-  terminalStore.set({
-  	"code" : cm.getValue(), 
-  	"history" : cm.getHistory(),
-  	"cursor"  : cm.getCursor(),
-  	"terminal"    : terminal.getValue()
-  });
-};
+	function saveCode(){
+		terminalStore.set({
+			"code" : cm.getValue(), 
+			"history" : cm.getHistory(),
+			"cursor"  : cm.getCursor(),
+			"terminal"    : terminal.getValue()
+		});
+	};
 
 </script>
 <style>
@@ -86,7 +85,7 @@ function saveCode(){
 		height: var(--box-margin);
 	}
 	.footer {
-  		flex: 0 1 40px;
+		flex-grow: 0 1;
 	}
 	.wrap {
 		margin-left: var(--box-margin);
@@ -95,46 +94,44 @@ function saveCode(){
 		margin-top: .5em;
 		display:flex;
 		align-items:center;
-		width:100%;
 		justify-content: space-between;
 	}
-	.wrap div:last-child {
-		margin-left: auto;
-	}
-	.highlight {
+	.loopback {
+		flex-grow: 1;
+		height: 50vh;
 		background-color: var(--background-highlight);
+		padding-left: var(--box-margin);
 	}
+
 	.flex {
-           display: flex;
-           flex-flow: column;
-           height: 100vh;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
 	}
 
 </style>
 <div class="flex">
-<div out:saveCode></div>
-<HLHeader>
-	<div class="highlight">
+	<div out:saveCode></div>
+
+	<div class="loopback">
 		<CodeMirror name={"terminal"} on:initialized={setUpTerminal} />
 	</div>
-</HLHeader>
-<HLContent>
-	<HLBorder />
-	<div class="v-space"></div>
-	<CodeMirror name={"code"} on:initialized={setUpCM} />
-</HLContent>
 
-<div class="footer">
-	<HLRow>
+	<HLContent>
+		<HLBorder />
+		<div class="v-space"></div>
+		<CodeMirror name={"code"} on:initialized={setUpCM} />
+	</HLContent>
+
+	<div class="footer">
 		<div class="wrap">
-			<HLButton on:click={runRepl} style="padding-left: 6em; padding-right: 6em;">
+			<HLButton on:click={runRepl}>
 				Run
 			</HLButton>
-			<HLButton on:click={clearRepl} style="padding-left: 1.5em; padding-right: 1.5em;">
+			<HLButton on:click={clearRepl}>
 				Clear
 			</HLButton>
 		</div>
-	</HLRow>
-</div>
+	</div>
 
 </div>
