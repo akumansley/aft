62515d743d4a2570a0995fff4cf7186585871168
Andrew Wansley <andrew.wansley@gmail.com>
2020-10-20 21:47:45 -0700

    Add routing and a list view to Giftr

---
+++ client/example/giftr/src/List.svelte
@@ -0,0 +1,13 @@
+<script>
+	import client from './client.js';
+	import user from './user.js';
+	export let userId;
+	import { Link } from "svelte-routing";
+	
+</script>
+
+<Link to="/">&larr; Back</Link>
+<div class="box">
+	You are {$user.id}; this is the list of {userId}.
+</div>
+
