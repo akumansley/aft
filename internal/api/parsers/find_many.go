b31d1776ec057b9849e0b56175ec940943fe31a8
Andrew Wansley <andrew.wansley@gmail.com>
2021-01-13 16:32:19 -0800

    Minor refactor of the findMany parser

--- internal/api/parsers/find_many.go
+++ internal/api/parsers/find_many.go
@@ -1,44 +1,31 @@
 package parsers
 
 import (
+	"fmt"
+
 	"awans.org/aft/internal/api"
 	"awans.org/aft/internal/api/operations"
 	"awans.org/aft/internal/db"
-	"fmt"
 )
 
 func (p Parser) ParseFindMany(modelName string, args map[string]interface{}) (op operations.FindManyOperation, err error) {
-	m, where, inc, sel, ca, err := p.find(modelName, args)
+	m, fa, err := p.find(modelName, args)
 	if err != nil {
 		return
 	}
 	op = operations.FindManyOperation{
-		ModelID: m.ID(),
-		FindArgs: operations.FindArgs{
-			Where:   where,
-			Include: inc,
-			Select:  sel,
-			Case:    ca,
-		},
+		ModelID:  m.ID(),
+		FindArgs: fa,
 	}
 	return
 }
 
 func (p Parser) parseNestedFindMany(modelName string, args map[string]interface{}) (op operations.FindArgs, err error) {
-	_, where, inc, sel, ca, err := p.find(modelName, args)
-	if err != nil {
-		return
-	}
-	op = operations.FindArgs{
-		Where:   where,
-		Include: inc,
-		Select:  sel,
-		Case:    ca,
-	}
-	return
+	_, fa, err := p.find(modelName, args)
+	return fa, err
 }
 
-func (p Parser) find(modelName string, args map[string]interface{}) (m db.Interface, where operations.Where, inc operations.Include, sel operations.Select, ca operations.Case, err error) {
+func (p Parser) find(modelName string, args map[string]interface{}) (m db.Interface, fa operations.FindArgs, err error) {
 	m, err = p.Tx.Schema().GetInterface(modelName)
 	if err != nil {
 		return
@@ -49,24 +36,31 @@ func (p Parser) find(modelName string, args map[string]interface{}) (m db.Interf
 		unusedKeys[k] = api.Void{}
 	}
 
-	where, err = p.consumeWhere(m, unusedKeys, args)
+	where, err := p.consumeWhere(m, unusedKeys, args)
 	if err != nil {
 		return
 	}
 
-	inc, sel, err = p.consumeIncludeOrSelect(m, unusedKeys, args)
+	inc, sel, err := p.consumeIncludeOrSelect(m, unusedKeys, args)
 	if err != nil {
 		return
 	}
 
-	ca, err = p.consumeCase(m, unusedKeys, args)
+	ca, err := p.consumeCase(m, unusedKeys, args)
 	if err != nil {
 		return
+	}
+
+	fa = operations.FindArgs{
+		Where:   where,
+		Include: inc,
+		Select:  sel,
+		Case:    ca,
 	}
 
 	if len(unusedKeys) != 0 {
-		return m, where, inc, sel, ca, fmt.Errorf("%w: %v", ErrUnusedKeys, unusedKeys)
+		return m, fa, fmt.Errorf("%w: %v", ErrUnusedKeys, unusedKeys)
 	}
 
-	return m, where, inc, sel, ca, err
+	return
 }
