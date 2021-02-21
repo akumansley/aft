9261be5f4af6d6ccd2547d864ca68c2d8b188c65
Andrew Wansley <andrew.wansley@gmail.com>
2021-02-20 17:37:23 -0800

    Add better CSRF protection

--- client/catalog/src/data/client.js
+++ client/catalog/src/data/client.js
@@ -1,6 +1,16 @@
 const basePath = "/";
 const methods = ["create", "findOne", "findMany", "update", "updateMany", "count", "delete", "deleteMany", "upsert"];
 
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) {
+    return parts.pop().split(";").shift();
+  }
+  return ""
+};
+
 async function call(path, params) {
   if(typeof params === 'undefined')  {
     params = {};
@@ -8,7 +18,7 @@ async function call(path, params) {
   const res = await fetch(basePath + path, {
     method: "POST",
     body: JSON.stringify(params),
-    headers: {"Content-Type": "application/json"},
+    headers: {"X-CSRF": getCookie("csrf")},
   });
   const responseBody = await res.json();
   if ("code" in responseBody) {
--- internal/csrf/csrf.go
+++ internal/csrf/csrf.go
@@ -3,9 +3,11 @@ package csrf
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"awans.org/aft/internal/db"
 	"awans.org/aft/internal/server/lib"
+	"github.com/google/uuid"
 )
 
 type Module struct {
@@ -28,12 +30,52 @@ func (m *Module) ProvideMiddleware() []lib.Middleware {
 	return []lib.Middleware{CSRF}
 }
 
+var ttl = 30 * time.Minute
+
+func getCSRFCookie(r *http.Request) (string, bool) {
+	csrfCookie, err := r.Cookie("csrf")
+	if err != nil || csrfCookie.Value == "" {
+		return "", false
+	}
+	return csrfCookie.Value, true
+}
+
+func getCSRFHeader(r *http.Request) (string, bool) {
+	val := r.Header.Get("X-CSRF")
+	return val, val != ""
+}
+
+func setCSRFCookie(w http.ResponseWriter) {
+	expires := time.Now().Add(ttl)
+	val := uuid.New()
+
+	c := &http.Cookie{
+		Name:     "csrf",
+		Value:    val.String(),
+		Expires:  expires,
+		Domain:   "",
+		Path:     "/",
+		HttpOnly: false,
+		SameSite: http.SameSiteStrictMode,
+	}
+	http.SetCookie(w, c)
+}
+
 func CSRF(inner http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cType := r.Header.Get("Content-Type")
-		if cType != "application/json" && r.Method != "GET" {
-			lib.WriteError(w, errors.New("Invalid Content-Type; must be application/json"))
+		cookieVal, set := getCSRFCookie(r)
+		if !set {
+			setCSRFCookie(w)
+		}
+		if r.Method != "GET" && !set {
+			lib.WriteError(w, errors.New("Invalid request; must include CSRF cookie"))
 			return
+		} else if r.Method != "GET" {
+			headerVal, set := getCSRFHeader(r)
+			if !set || (headerVal != cookieVal) {
+				lib.WriteError(w, errors.New("Invalid request; CSRF cookie didn't match X-CSRF header"))
+				return
+			}
 		}
 		inner.ServeHTTP(w, r)
 	})
